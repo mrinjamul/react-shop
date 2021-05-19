9875fb7a4406d6e66a027d3c2fdcab0b978d924a
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2021-05-19 13:56:20 +0530

    tailwind without custom css

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div class="chat-notification">
-        <div class="chat-notification-logo-wrapper">
-          <img class="chat-notification-logo" src={logo} alt="ChitChat Logo" />
+      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div class="flex-shrink-0">
+          <img class="h-12 w-12" src={logo} alt="ChitChat Logo" />
         </div>
-        <div class="chat-notification-content">
-          <h4 class="chat-notification-title">ChitChat</h4>
-          <p class="chat-notification-message">You have a new message!</p>
+        <div>
+          <div class="text-xl font-medium text-black">ChitChat</div>
+          <p class="text-gray-500">You have a new message!</p>
         </div>
       </div>
     </div>
