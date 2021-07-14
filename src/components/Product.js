9b5627ea66dbe41f4dc20a24f72c42905061742e
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2021-07-14 17:11:39 +0530

    Add warpper div to set padding 2rem

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,12 @@
 function Product({ product: { title, price, description, category, image } }) {
   return (
     <div className="flex bg-white w-full mb-5">
-      <div
-        style={{ backgroundImage: `url(${image})` }}
-        className="w-5/12 bg-contain bg-no-repeat bg-center"
-      ></div>
+      <div className="w-5/12 p-2">
+        <div
+          style={{ backgroundImage: `url(${image})` }}
+          className="bg-contain bg-no-repeat bg-center w-full h-full"
+        ></div>
+      </div>
       <div className="w-7/12 p-5">
         <h1 className="md:text-2xl">{title.substr(0, 50)}</h1>
         <h3 className="text-gray-400">{category}</h3>
