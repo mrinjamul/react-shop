e5172c282f8c6f8fdfe2a3948132824ffe3973ba
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2021-07-14 16:50:31 +0530

    when min-width is ^768px width is set to 1/2 (50%) and mx-auto means center the content

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import products from "./data/products.json";
 
 function App() {
   return (
-    <div>
-      <div>
-        {products.map((product) => (
-          <Product product={product} key={product.id} />
+    <div className="bg-gray-200">
+      <div className="md:w-1/2 mx-auto">
+        {products.map((product, id) => (
+          <Product product={product} key={id} />
         ))}
       </div>
     </div>
