1570c929c60274a4bd18aacd986fb653bf85dcdb
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2021-07-14 17:26:21 +0530

    add a white background to the background wrapper when it’s in dark mode.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 function Product({ product: { title, price, description, category, image } }) {
   return (
     <div className="flex bg-white w-full mb-5 shadow-sm rounded-lg dark:bg-gray-300">
-      <div className="w-5/12 p-2">
+      <div className="w-5/12 p-2 dark:bg-white rounded-tl-lg rounded-bl-lg">
         <div
           style={{ backgroundImage: `url(${image})` }}
           className="bg-contain bg-no-repeat bg-center w-full h-full"
@@ -9,7 +9,7 @@ function Product({ product: { title, price, description, category, image } }) {
       </div>
       <div className="w-7/12 p-5">
         <h1 className="md:text-2xl">{title.substr(0, 50)}</h1>
-        <h3 className="text-gray-400">{category}</h3>
+        <h3 className="text-gray-400 dark:text-gray-700">{category}</h3>
         <p className="text-red-400 mt-4 text-xl md:text-4xl">{price}$</p>
         <div className="mt-4">{description.substr(0, 100)}</div>
       </div>
