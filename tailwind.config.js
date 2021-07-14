6190a014aaa8f8f3ef5738841b8f740d1adf272c
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2021-07-14 17:17:37 +0530

    Add color palettes from tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        turquoise: "#40e0d0",
+        gray: colors.blueGray,
+        red: colors.rose,
       },
     },
   },
