b495ffff8fefdef237ebac7e32fb93f1e35df8dd
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2021-07-15 12:14:37 +0530

    Add: darkmode toggle button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./App.css";
 import NavigationBar from "./components/NavigationBar";
 import Product from "./components/Product";
@@ -5,9 +6,18 @@ import products from "./data/products.json";
 import logo from "./logo.svg";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const setDark = () => {
+    setDarkMode(!darkMode);
+  };
   return (
-    <div>
-      <NavigationBar title="React Shop Example" logo={logo} />
+    <div className={`w-full md:h-screen ${darkMode ? "dark" : ""}`}>
+      <NavigationBar
+        title="React Shop Example"
+        logo={logo}
+        darkMode={darkMode}
+        setDark={setDark}
+      />
       <div className="bg-gray-200 py-4 dark:bg-gray-800">
         <div className="md:w-1/2 mx-auto">
           {products.map((product, id) => (
